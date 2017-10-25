4a765c689d121f843c04933208d6152e97b23a14
Maral Abdreissov <maral_957@mail.ru>
2017-10-25 17:35:20 +0600

    Installed and Configured babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-const gulp = require('gulp');
+const	gulp	= require('gulp'),
+		babel	= require('gulp-babel');
 
 gulp.task('default', function(){
-	console.log('All is good');
+	//	Исходный код для Node.js
+	gulp.src('es6/**/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('dist'));
+	//	Исходный код для браузера
+	gulp.src('public/es6/**/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('public/dist/'));
 });
