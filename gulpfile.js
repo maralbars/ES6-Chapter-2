19ee598a0212246d23e855e0cc7748a9cabf8aee
Maral Abdreissov <maral_957@mail.ru>
2017-12-13 12:02:18 +0600

    Add lint to es6-pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const	gulp	= require('gulp'),
 		babel	= require('gulp-babel');
 
 gulp.task('default', function(){
+	//	запуск ESLin t
+	gulp.src(["es6/**/*.js", "pubic/es6/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
 	//	Исходный код для Node.js
 	gulp.src('es6/**/*.js')
 		.pipe(babel())
